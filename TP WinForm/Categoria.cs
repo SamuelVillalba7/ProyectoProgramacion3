3f32f7f466ad07020938a6f92cdcc31cf6207caa
Samuel Villalba <villalbasamuel2004@gmail.com>
2024-09-04 14:05:45 -0300

    creacion de clases

---
+++ TP WinForm/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm
+{
+    internal class Categoria
+    {
+        private int IDCategoria;
+        private string Nombre;
+
+    }
+}
---
+++ TP WinForm/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm
+{
+    internal class Imagen
+    {
+        private int IDImagen;
+        private int IDArticulo;
+        private string ImagenUrl;
+    }
+}
